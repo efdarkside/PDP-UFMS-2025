951665f7b2ae1ffb5264d5c0ac521ed1260212c6
efdarkside <100876240+efdarkside@users.noreply.github.com>
2026-04-10 10:53:05 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './PDP_2025_Dashboard.jsx'
+import App from './PDP_2025_Dashboard.jsx' // Aqui ele importa o 'default' que você achou
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
