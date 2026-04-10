fa65c6595147e8b749a80d132b7f313193bc03a1
efdarkside <100876240+efdarkside@users.noreply.github.com>
2026-04-10 10:14:04 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nome-do-seu-repositorio/', // ADICIONE ISSO: O nome do seu repositório entre barras
+  base: '/PDP-UFMS-2025/', // ADICIONE ISSO: O nome do seu repositório entre barras
 })
